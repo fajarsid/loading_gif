bf89eadad6350613a55b0b4adc0f7bc1f0829bdb
fajarsid <fajarsuganda79@gmail.com>
2021-04-14 10:03:52 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:splashscreen/splashscreen.dart';
+
+void main() {
+  runApp(new MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: new MyApp(),
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return new SplashScreen(
+        seconds: 10,
+        image: new Image.asset('assets/loading.gif'),
+        backgroundColor: Colors.black,
+        photoSize: 150.0,
+        onClick: () => print('Loading Gif'),
+        loaderColor: Colors.black);
+  }
+}
